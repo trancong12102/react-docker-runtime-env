7d7939ef3a5c24be6d7fe85993f86752ae95a854
Công Trần <trancong12102@gmail.com>
2024-12-25 10:09:34 +0700

    feat: prefix value

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,27 +12,24 @@ echo "Found env names: $env_names"
 
 # find all js files in dist
 js_files=$(find $dist_path -type f -name "*.js")
-echo "Found js files: $js_files"
 
 # loop through all js files
 for js_file in $js_files; do
-  echo "Processing $js_file"
 
   # loop through all env names
   for env_name in $env_names; do
     env_name=$env_prefix$env_name
     # get env value by env name from runtime env
     env_value=$(printenv $env_name)
-    echo "Found env value: $env_value"
 
     # skip if env value is empty
     if [ -z "$env_value" ]; then
-      echo "Skip $env_name"
       continue
     fi
 
     # replace string "$env_name" with "$env_value" in js file
-    sed -i "s/$env_name/$env_value/g" $js_file
+    sed -i "s,__$env_name,$env_value,g" $js_file
+    echo "Replaced $env_name with $env_value in $js_file"
   done
 done
 
