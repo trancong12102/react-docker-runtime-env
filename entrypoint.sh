6eac8b715cf4af3316d3109733002f1ea877b01d
Công Trần <trancong12102@gmail.com>
2025-02-11 17:32:06 +0700

    fix: remove quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,7 @@ env_runtime_lines=$(awk '/### RUNTIME ###/ {getline; if($0 ~ /=/) print}' "$env_
 for line in $env_runtime_lines; do
   env_name=$(echo "$line" | cut -d '=' -f 1)
   env_value=$(echo "$line" | cut -d '=' -f 2 | sed 's/\s*#.*//')
+  env_value=$(echo "$env_value" | sed 's/^"\([^"]*\)"$/\1/; s/^'\''\([^'\'']*\)'\''$/\1/')
   runtime_value=$(printenv "$env_name")
 
   echo "env_name: $env_name"
@@ -29,7 +30,6 @@ for line in $env_runtime_lines; do
   # loop through all js files
   for js_file in $js_files; do
     $() sed -i "s,$env_value,$runtime_value,g" "$js_file"
-    echo "Replaced $env_value with $runtime_value in $js_file"
   done
 done
 
