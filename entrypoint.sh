d4addf5525818cb8862ab1452199b1fc420371b5
Công Trần <trancong12102@gmail.com>
2025-02-11 16:54:13 +0700

    feat: update to find by env_value

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,30 +6,30 @@ env_path=$(pwd)/.env.production
 # dist path
 dist_path=$(pwd)
 
-# get all env variable names from env_path (skip empty lines, comments)
-env_names=$(grep -v '^\s*$\|^\s*#' $env_path | cut -d '=' -f 1)
-echo "Found env names: $env_names"
-
 # find all js files in dist
 js_files=$(find $dist_path -type f -name "*.js")
 
-# loop through all js files
-for js_file in $js_files; do
+# get all runtime env variable lines from env_path
+env_runtime_lines=$(awk '/### RUNTIME ###/ {getline; if($0 ~ /=/) print}' "$env_path")
 
-  # loop through all env names
-  for env_name in $env_names; do
-    env_name=$env_prefix$env_name
-    # get env value by env name from runtime env
-    env_value=$(printenv $env_name)
+# loop through all runtime env variable lines
+for line in $env_runtime_lines; do
+  env_name=$(echo "$line" | cut -d '=' -f 1)
+  env_value=$(echo "$line" | cut -d '=' -f 2 | sed 's/\s*#.*//')
+  runtime_value=$(printenv "$env_name")
 
-    # skip if env value is empty
-    if [ -z "$env_value" ]; then
-      continue
-    fi
+  echo "env_name: $env_name"
+  echo "env_value: $env_value"
 
-    # replace string "$env_name" with "$env_value" in js file
-    sed -i "s,__$env_name,$env_value,g" $js_file
-    echo "Replaced $env_name with $env_value in $js_file"
+  # skip if runtime value is empty
+  [ -z "$runtime_value" ] && continue
+
+  echo "Replacing $env_value with $runtime_value in all js files"
+
+  # loop through all js files
+  for js_file in $js_files; do
+    $() sed -i "s,$env_value,$runtime_value,g" "$js_file"
+    echo "Replaced $env_value with $runtime_value in $js_file"
   done
 done
 
